36daf00a104b68ff321bd68c1a2abdf73c1856f8
riccardozulla <riccardozulla@gmail.com>
2022-12-19 12:16:38 +0100

    tests modified

--- TestStuffOnStreets.py
+++ TestStuffOnStreets.py
@@ -16,7 +16,7 @@ class TestStreetOwner():
         self.Player2 = self.game.players[1]
         
         
-        self.results = [5,5,11,5,9,11,5, 7]
+        self.results = [5,5,11,5,9,11,5, 7, 6]
 
         if(test == 0): #green color
             Move.placeFreeStreet(self.Player1, (41,49))
@@ -119,13 +119,24 @@ class TestStreetOwner():
             Move.placeFreeStreet(self.Player1, (39,47)) 
             os.system('cls' if os.name == 'nt' else 'clear')
             Move.placeFreeStreet(self.Player1, (30,40)) 
+        
+        if(test == 8):
+            Move.placeFreeStreet(self.Player1, (39,40))
+            Move.placeFreeStreet(self.Player1, (40,41))
+            Move.placeFreeStreet(self.Player1, (41,49))
+            Move.placeFreeStreet(self.Player1, (48,49))
+            Move.placeFreeStreet(self.Player1, (47,48))  
+            os.system('cls' if os.name == 'nt' else 'clear')
+            Move.placeFreeStreet(self.Player1, (39,47)) 
 
         # assert self.game.longestStreetLength == results[test], f'Was: {str(self.game.longestStreetLength)} Aspected: {str(results[test])}'
 
-for n in [0,1,2,7]:
-    ts = TestStreetOwner(n)
-    print(ts.game.findLeaves(ts.Player1))
-    assert ts.game.longestStreetLength == ts.results[n] , f'Was {ts.game.longestStreetLength} espected: {ts.results[n]}'
+# for n in [0,1,2,7]:
+n=8
+ts = TestStreetOwner(n)
+print(ts.game.findLeaves(ts.Player1))
+print(ts.game.connectedPlacesToPlace(ts.Player1, 49))
+assert ts.game.longestStreetLength == ts.results[n] , f'Was {ts.game.longestStreetLength} espected: {ts.results[n]}'
     
 
 # print(ts.game.findLeaves(ts.Player1))
