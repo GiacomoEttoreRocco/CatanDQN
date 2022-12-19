7ecf7548dcee621c293a12a1746e0a0db73d3f7c
riccardozulla <riccardozulla@gmail.com>
2022-12-19 16:57:59 +0100

    TestRoadBuilding with unittest

--- testStuffOnRoadBuilding.py
+++ testStuffOnRoadBuilding.py
@@ -3,39 +3,39 @@ import Classes.Board as Board
 import Classes.Bank as Bank
 import Classes.Move as Move
 import Classes.Game as Game
-import os
+import unittest
 
-class TestRoadBuilding():    
+class TestRoadBuilding(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+            super(TestRoadBuilding, self).__init__(*args, **kwargs)
+            self.initializeStuff()
 
-    def __init__(self, test):
-        print("Evaluating test: ", test)
+
+    def initializeStuff(self):
         self.game = Game.Game(2)
         
         self.Player1 = self.game.players[0]
         self.Player2 = self.game.players[1]
-        
-        
-        self.results = []
 
-        #Check if player can build streets when they're out of space
-        #To buy: buyDevCard(player, card, undo = False):
+    def test_1(self):
+        previous =  list.copy(self.Player1.ownedStreets)
+        Move.useRoadBuildingCard(self.Player1, [(0, 1), (0, 8)])
+        self.assertEqual(len(previous) + 2, len(self.Player1.ownedStreets))
+        self.assertTrue((0, 1) in self.Player1.ownedStreets)
+        self.assertTrue((0, 8) in self.Player1.ownedStreets)
 
-        if (test == 0):     #Generic placement
-            print("Owned streets before usage: ", self.Player1.ownedStreets)
-            Move.useRoadBuildingCard(self.Player1, [(0, 1), (0, 8)])
-            print("Owned streets after usage: ", self.Player1.ownedStreets)
-
-        if (test == 1):
-            edges = list(Board.Board().edges.keys())
-            for i in range(14):
-                Move.placeFreeStreet(self.Player1, edges[i])
-            print(self.Player1.ownedStreets)
-            Move.useRoadBuildingCard(self.Player1, [(10, 11), (11, 12)])
-            print(self.Player1.ownedStreets)
-            print(self.Player1.nStreets)
+    # def test_2(self):
+    #     edges = list(Board.Board().edges.keys())
+    #     for i in range(14):
+    #         Move.placeFreeStreet(self.Player1, edges[i])
+    #     print(self.Player1.ownedStreets)
+    #     Move.useRoadBuildingCard(self.Player1, [(10, 11), (11, 12)])
+    #     print(self.Player1.ownedStreets)
+    #     print(self.Player1.nStreets)
 
 
 
 
-n = 1
-TestRoadBuilding(n)
+
+if __name__ == '__main__':
+    unittest.main()
