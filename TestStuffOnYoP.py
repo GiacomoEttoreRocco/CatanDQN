f1d9df1d5522b0f2db26bdead4198835a7d4f201
unknown <giacomoettore.rocco@gmail.com>
2022-12-19 16:21:03 +0100

    you test

--- TestStuffOnYoP.py
+++ TestStuffOnYoP.py
@@ -27,21 +27,15 @@ class TestYearOfPlenty():
                 Bank.Bank().giveResource(self.Player2, resource)  #Player2 just stacks resources for test purposes
             print("Resource amount after bank emptied: ", Bank.Bank().resources)
 
-        def allowPlayerToBuyCard(self, player):
-            print(player.resources)
-            player.resources["iron"] += 1
-            player.resources["crop"] += 1
-            player.resources["sheep"] += 1
-            print(player.resources)
-
-
         if(test == 0):  #Generally tests if it works for two different resources
             print("--Before request--")
             print("Resource on", str(self.Player1.id), "for first resource :" , self.Player1.resources["crop"])
             print("Resource on", str(self.Player1.id), "for second resource :" , self.Player1.resources["iron"])
             print("Resource left on bank for first resource :" , Bank.Bank().resources["crop"])
             print("Resource left on bank for first resource :" , Bank.Bank().resources["iron"])
+
             Move.useYearOfPlentyCard(self.Player1, ["crop", "iron"])
+
             print("--After request--")
             print("Resource on", str(self.Player1.id), "for first resource :" , self.Player1.resources["crop"])
             print("Resource on", str(self.Player1.id), "for second resource :" , self.Player1.resources["iron"])
@@ -65,7 +59,6 @@ class TestYearOfPlenty():
             print("--After request--")
             print("Resource on player ", str(self.Player1.id), "is :" , self.Player1.resources["crop"])
 
-
         if(test == 3):   #Tests that card doesn't work when Bank is out of resources for two different resources
             emptyBank(self, "crop")
             print("--Before request--")
@@ -77,7 +70,6 @@ class TestYearOfPlenty():
             print("Second resource on player ", str(self.Player1.id), "is: " , self.Player1.resources["iron"])
 
         if(test == 4):  #Tests if amount of yop cards is decreased after usage
-            allowPlayerToBuyCard(self, self.Player1)
             print("Amount of yop cards before purchase: ", self.Player1.yearOfPlentyCard)
             Move.buyDevCard(self.Player1, "year_of_plenty")
             self.Player1.yearOfPlentyCard += 1  #Since doTurn is not being called, we simulate the increase
@@ -85,5 +77,6 @@ class TestYearOfPlenty():
             Move.useYearOfPlentyCard(self.Player1, ["crop", "crop"])
             print("Amount of yop cards after usage: ", self.Player1.yearOfPlentyCard)
     
-n = 4
+n = 0
+
 TestYearOfPlenty(n)
