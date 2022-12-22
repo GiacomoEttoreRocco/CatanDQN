0492c6acbafc96b1289f9871030487a29075e912
unknown <giacomoettore.rocco@gmail.com>
2022-12-22 15:38:52 +0100

    __init__.py di classes modificato

--- Classes/__init__.py
+++ Classes/__init__.py
@@ -1,6 +1,13 @@
-import Classes.Move as Move
-import Classes.Player as Player
-import Classes.Game as Game
-import Classes.Bank as Bank
-import Classes.Board as Board
-import Classes.CatanGraph as CatanGraph
+# import Classes.Move as Move
+# import Classes.Player as Player
+# import Classes.Game as Game
+# import Classes.Bank as Bank
+# import Classes.Board as Board
+# import Classes.CatanGraph as CatanGraph
+
+from Classes import Move
+from Classes import Player
+from Classes import Game
+from Classes import Bank
+from Classes import Board
+from Classes import CatanGraph
