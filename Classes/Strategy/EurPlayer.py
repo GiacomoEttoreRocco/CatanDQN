b63289528866d1fdfa2d51434e63231bb1610032
GiacomoEttoreRocco <giacomoettore.rocco@gmail.com>
2023-06-22 15:40:27 +0200

    bug

--- Classes/Strategy/EurPlayer.py
+++ Classes/Strategy/EurPlayer.py
@@ -7,11 +7,6 @@ from RL.DQGNN import DQGNNagent
 import random
 
 class EuristicPlayer(StrategyEuristic):
-    # def __init__(self): 
-        # print("RANDOM EURISTIC CONSTRUCTOR")
-        # self.macroDQN = DQGNNagent(11, 10) 
-        # self.eps = self.macroDQN.EPS
-
     def name(self):
         return "REUR"
 
@@ -21,13 +16,9 @@ class EuristicPlayer(StrategyEuristic):
         elif(player.game.actualTurn<player.game.nplayers*2):
             return self.chooseParameters(commands.SecondChoiseCommand, player)
         else:
-            # graph = Board.Board().boardStateGraph(player)
-            # glob = player.globalFeaturesToTensor()
-            # RICORDATI CHE VANNO GESTITE LE FORCED MOVES, in futuro.
             idActions = player.availableTurnActionsId()
             if(len(idActions) == 1 and idActions[0] == 0):
                 return commands.PassTurnCommand, None, True
-            # bestMove = self.macroDQN.step(graph, glob, player.availableTurnActionsId()) 
             bestMove = random.choice(idActions)
-        return self.chooseParameters(idToCommand(bestMove), player) # bestAction, thingsNeeded, onlyPassTurn
+        return self.chooseParameters(idToCommand(bestMove), player) 
         
--- Classes/Strategy/RanPlayer.py
+++ Classes/Strategy/RanPlayer.py
@@ -6,11 +6,6 @@ from Command import commands, controller
 import random
 
 class RandomPlayer(StrategyRandom):
-    # def __init__(self): 
-        # print("RANDOM EURISTIC CONSTRUCTOR")
-        # self.macroDQN = DQGNNagent(11, 10) 
-        # self.eps = self.macroDQN.EPS
-
     def name(self):
         return "REUR"
 
@@ -20,13 +15,9 @@ class RandomPlayer(StrategyRandom):
         elif(player.game.actualTurn<player.game.nplayers*2):
             return self.chooseParameters(commands.SecondChoiseCommand, player)
         else:
-            # graph = Board.Board().boardStateGraph(player)
-            # glob = player.globalFeaturesToTensor()
-            # RICORDATI CHE VANNO GESTITE LE FORCED MOVES, in futuro.
             idActions = player.availableTurnActionsId()
             if(len(idActions) == 1 and idActions[0] == 0):
                 return commands.PassTurnCommand, None, True
-            # bestMove = self.macroDQN.step(graph, glob, player.availableTurnActionsId()) 
             bestMove = random.choice(idActions)
-        return self.chooseParameters(idToCommand(bestMove), player) # bestAction, thingsNeeded, onlyPassTurn
+        return self.chooseParameters(idToCommand(bestMove), player) 
         
