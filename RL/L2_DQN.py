8b5cc94dddedf7aaf575258dc7cbc3c0b41fcc86
GiacomoEttoreRocco <giacomoettore.rocco@gmail.com>
2023-06-30 19:11:35 +0200

    eps = 0.999 hff

--- RL/L2_DQN.py
+++ RL/L2_DQN.py
@@ -30,7 +30,7 @@ class L2DQNagent():
         self.optimizer = torch.optim.AdamW(self.policy_net.parameters(), lr=self.LearningRate)
         self.memory = ReplayMemory(1000)
 
-        self.decay = 0.996
+        self.decay = 0.999
 
     def epsDecay(self):
         self.EPS = self.EPS * self.decay
--- epsCalc.py
+++ epsCalc.py
@@ -1,6 +1,6 @@
 e = 1
-for i in range(0,10000):
-    e = e*0.9996
+for i in range(0,3000):
+    e = e*0.999
     if(i%100 == 0 and i > 0):
         print(e)
 
